53bd3e128eeabe505191824dbfe8de3e8b05f8a0
Vineet-Sharma29 <vineet.s17@iiits.in>
2018-10-24 15:45:52 +0530

    removed unnecessary variables

--- canny.py
+++ canny.py
@@ -3,13 +3,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img_1 = cv2.imread('4.png',0)
+plt.subplot(331),plt.imshow(img_1,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+
 ret, img = cv2.threshold(img_1,127,255,cv2.THRESH_BINARY)
+plt.subplot(332),plt.imshow(img,cmap = 'gray')
+plt.title('Threshold Image'), plt.xticks([]), plt.yticks([])
+
 img_1_m = cv2.medianBlur(img,5)
+plt.subplot(333),plt.imshow(img_1,cmap = 'gray')
+plt.title('Blur Image'), plt.xticks([]), plt.yticks([])
+
 edges = cv2.Canny(img_1_m,200,300)
+plt.subplot(334),plt.imshow(edges,cmap = 'gray')
+plt.title('Canny Image'), plt.xticks([]), plt.yticks([])
 
 
-img_2 = cv2.medianBlur(edges,5) 
-cimg = cv2.cvtColor(img_2,cv2.COLOR_GRAY2BGR)
 
 circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
                             param1=50,param2=30,minRadius=0,maxRadius=0)
@@ -21,6 +30,9 @@ for i in circles[0,:]:
     # draw the center of the circle
     cv2.circle(img_1,(i[0],i[1]),2,(255,255,255),3)
     break
+
+plt.subplot(335),plt.imshow(img_1,cmap = 'gray')
+plt.title('Hough Tranform '), plt.xticks([]), plt.yticks([])
 '''
 cv2.imshow('detected circles',cimg)
 cv2.waitKey(0)
@@ -28,23 +40,21 @@ cv2.destroyAllWindows()
 
 '''
 
+
 img_1_1 = cv2.imread('4.png',0)
+
 '''
 ret, img = cv2.threshold(img_1,127,255,cv2.THRESH_BINARY)
 edges = cv2.Canny(img,200,300)
 '''
 img = cv2.Canny(img_1_1,200,300)
+plt.subplot(336),plt.imshow(img,cmap = 'gray')
+plt.title('Canny Image _ 2'), plt.xticks([]), plt.yticks([])
+
 ret, edges = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+plt.subplot(337),plt.imshow(edges,cmap = 'gray')
+plt.title('Threshold Image _ 2'), plt.xticks([]), plt.yticks([])
 
-
-
-
-plt.subplot(131),plt.imshow(edges,cmap = 'gray')
-plt.title('Edges Image'), plt.xticks([]), plt.yticks([])
-
-
-img_2 = cv2.medianBlur(edges,5)
-cimg = cv2.cvtColor(img_2,cv2.COLOR_GRAY2BGR)
 
 circles = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,1,20,
                             param1=50,param2=30,minRadius=0,maxRadius=0)
@@ -52,20 +62,13 @@ circles = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,1,20,
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
     # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(255,255,255),2)
     cv2.circle(img_1,(i[0],i[1]),i[2],(255,255,255),2)
     # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(255,255,255),3)
     cv2.circle(img_1,(i[0],i[1]),2,(255,255,255),3)
 
 
-
-
-
-plt.subplot(121),plt.imshow(edges,cmap = 'gray')
-plt.title('Canny Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(img_1,cmap = 'gray')
-plt.title('Hough Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(338),plt.imshow(img_1,cmap = 'gray')
+plt.title('Hough Image Final'), plt.xticks([]), plt.yticks([])
 
 plt.show()
 
