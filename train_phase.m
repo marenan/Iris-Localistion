589b883a76b8467540fbeb262101bea888ac1d09
Vineet Sharma <vineet.s17@iiits.in>
2018-11-12 01:42:56 +0530

    Rearranged train_phase.m to matlan

--- train_phase.m
+++
@@ -1,8 +0,0 @@
-positive_ins = pos_roi_2_b;
-pos_dir = fullfile('C:\Users\Vineet\Desktop\me\sem-3\DSAA\cascader\positive');
-addpath(pos_dir);
-neg_dir = fullfile('C:\Users\Vineet\Desktop\me\sem-3\DSAA\cascader\negative');
-
-trainCascadeObjectDetector('trained_model.xml',positive_ins,neg_dir,...
-    'NumCascadeStages',10,'FeatureType','Haar');
-
