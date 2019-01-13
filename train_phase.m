6bd1f0c80d6e5839954d3c7c5b3d2eb5617dc1e7
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-01-13 22:23:45 +0530

    Made changes in train_phase.m

--- train_phase.m
+++ train_phase.m
@@ -1,8 +1,7 @@
-positive_ins = pos_roi_2_b;
+positive_ins = objectDetectorTrainingData(gTruth);
 pos_dir = fullfile('C:\Users\Vineet\Desktop\me\sem-3\DSAA\cascader\positive');
 addpath(pos_dir);
 neg_dir = fullfile('C:\Users\Vineet\Desktop\me\sem-3\DSAA\cascader\negative');
 
-trainCascadeObjectDetector('trained_model.xml',positive_ins,neg_dir,...
+trainCascadeObjectDetector('trained_model_1.xml',positive_ins,neg_dir,...
     'NumCascadeStages',10,'FeatureType','Haar');
-
