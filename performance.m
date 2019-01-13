4615488c2edc2bac4f46ae533eba9d5da0e94c09
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-01-13 22:24:24 +0530

    Made changes in performance.m

--- performance.m
+++ performance.m
@@ -1,11 +1,14 @@
 detector = vision.CascadeObjectDetector('trained_model.xml');
-
 imgSets = imageSet('test','recursive');
-n =  uint32([trd.Count]);
-for k=1:10
+for k=11:21
    img = read(imgSets(1),k);
-   bbox = step(detector,img);
-   detectedImg = insertObjectAnnotation(img,'rectangle',bbox,'iris');
+   img = rgb2gray(img);
+   J = imnoise(img,'salt & pepper',0.02);
+   J = filter2(fspecial('average',3),J)/255;
+   J = medfilt2(J);
+   J = imnoise(J,'gaussian',0,0.025);
+   bbox = step(detector,J);
+   detectedImg = insertObjectAnnotation(J,'rectangle',bbox,'iris');
    figure; imshow(detectedImg);
 end
 
